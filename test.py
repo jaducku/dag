1e5228ce9c2561e19211dac3cf8b2a7c830915d6
jaducku <jaducku@gmail.com>
2024-08-02 21:46:04 +0900

    3

--- test.py
+++ test.py
@@ -1,28 +1,15 @@
-from airflow import DAG
-from airflow.operators.bash import BashOperator
-from datetime import datetime
+from airflow import models, settings
+from airflow.contrib.auth.backends.password_auth import PasswordUser
+User = PasswordUser(models.User())
 
-# DAG 설정
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-}
+# User Info
+user.username = 'minsupark'
+user.email = 'minsu@korea.com'
+user.password = 'mypassword1234'
 
-dag = DAG(
-    'triggered_jar_execution',
-    default_args=default_args,
-    description='A DAG that runs a JAR file triggered externally',
-    schedule_interval=None,  # 스케줄링 없음
-    catchup=False,
-)
-
-# BashOperator를 사용하여 JAR 파일 실행
-run_jar = BashOperator(
-    task_id='run_jar',
-    bash_command='java -jar /path/to/your/file.jar',
-    dag=dag,
-)
+session = settings.Session()
+user_exits = session.query(models.User.id).filter_by(username=user.username).scalar() is not None
+if not user_exits:
+    session.add(user)
+    session.commit()
+session.close()
