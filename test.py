2e4bc84b60c2287f9506df17a1ea9dd2d7f78aa7
jaducku <jaducku@gmail.com>
2024-08-02 21:49:53 +0900

    4

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from airflow.contrib.auth.backends.password_auth import PasswordUser
 User = PasswordUser(models.User())
 
 # User Info
-user.username = 'minsupark'
-user.email = 'minsu@korea.com'
-user.password = 'mypassword1234'
+user.username = 'defect-rest'
+user.email = 'jaducku@kakao.com'
+user.password = '1234'
 
 session = settings.Session()
 user_exits = session.query(models.User.id).filter_by(username=user.username).scalar() is not None
