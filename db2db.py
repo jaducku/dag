9547afd08c3e943cd791fe6ed29202d124c3c490
jaducku <jaducku@gmail.com>
2024-08-10 08:50:44 +0900

    trfggf

--- db2db.py
+++ db2db.py
@@ -16,22 +16,13 @@ default_args = {
 }
 
 dag = DAG(
-    'branch_operator_skip_task_dag',
+    'db2db',
     default_args=default_args,
     description='A DAG to dynamically skip running tasks using BranchPythonOperator',
     schedule_interval=timedelta(minutes=10),
     start_date=days_ago(1),
     catchup=False,
 )
-
-def choose_branch(task_id, **kwargs):
-    ti = TaskInstance(kwargs['dag'].get_task(task_id), kwargs['execution_date'])
-    ti.refresh_from_db()
-
-    if ti.state in [State.RUNNING, State.QUEUED]:
-        return f'skip_{task_id}'
-    else:
-        return task_id
 
 def my_task(task_number, **kwargs):
     time.sleep(random.randint(40, 90))
@@ -46,24 +37,17 @@ start = DummyOperator(
 for i in range(10):
     task_id = f'task_{i}'
 
-    branch_task = BranchPythonOperator(
-        task_id=f'branch_{task_id}',
-        python_callable=choose_branch,
-        op_args=[task_id],
-        provide_context=True,
-        dag=dag,
-    )
-
-    task = PythonOperator(
+    etl_main = PythonOperator(
         task_id=task_id,
         python_callable=my_task,
         op_kwargs={'task_number': i},
         dag=dag,
     )
 
-    skip_task = DummyOperator(
-        task_id=f'skip_{task_id}',
-        dag=dag,
-    )
+end = DummyOperator(
+    task_id='end',
+    dag=dag,
+    trigger_rule='all_done',
+)
 
-    start >> branch_task >> [task, skip_task]
+start >> etl_main >> end
