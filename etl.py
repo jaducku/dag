7dcc7669b67c8b10e48f58a897a6fb7b95f4a960
jaducku <jaducku@gmail.com>
2024-08-10 16:53:59 +0900

    dfd

--- etl.py
+++ etl.py
@@ -7,12 +7,13 @@ from airflow.exceptions import AirflowSkipException
 import time
 import random
 # 전체 Task 상태를 확인하고 딕셔너리에 저장하는 함수
+task_states = {}
 def check_all_task_states(**kwargs):
     dag_id = kwargs['dag'].dag_id
     current_execution_date = kwargs['execution_date']
     
     # 현재 DAG에서 실행 중인 Task들의 상태를 딕셔너리에 저장
-    task_states = {}
+    
     running_dag_runs = DagRun.find(dag_id=dag_id, state=State.RUNNING)
     
     for dag_run in running_dag_runs:
@@ -20,9 +21,6 @@ def check_all_task_states(**kwargs):
             task_instances = dag_run.get_task_instances()
             for task_instance in task_instances:
                 task_states[task_instance.task_id] = task_instance.state
-    
-    # 이 딕셔너리를 반환하여 다음 Task로 전달
-    return task_states
 
 # 개별 Task 실행 함수
 def execute_task_with_check(task_id, task_states, **kwargs):
@@ -55,8 +53,6 @@ with DAG('dag_task_state_check_no_xcom',
             task_id=task_id,
             python_callable=execute_task_with_check,
             op_args=[task_id],  # Task ID 전달
-            op_kwargs={"task_states": check_task_states.output},  # Task 상태 딕셔너리 전달
-            provide_context=True,
             dag=dag
         )
         tasks.append(task)
