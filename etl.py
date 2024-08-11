4af415ebfb206bfb7ecf34049706b2e62c4de021
jaducku <jaducku@gmail.com>
2024-08-11 09:20:00 +0900

    eere

--- etl.py
+++ etl.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.utils.db import provide_session
 from airflow.utils.dates import days_ago
 from airflow.models import DagRun, TaskInstance
 from airflow.utils.state import State
@@ -10,22 +11,22 @@ from datetime import datetime
 
 dag_name = 'task_check'
 # 전체 Task 상태를 확인하고 딕셔너리에 저장하는 함수
-def check_task_statuses(dag_id, execution_date=None):
+@provide_session
+def check_task_statuses(dag_id, execution_date=None, session=None):
     # execution_date가 없으면 가장 최근 실행을 조회
-    execution_date = execution_date or datetime.utcnow()
+    if execution_date is None:
+        dag_run = session.query(DagRun).filter(DagRun.dag_id == dag_id).order_by(DagRun.execution_date.desc()).first()
+    else:
+        dag_run = session.query(DagRun).filter(DagRun.dag_id == dag_id, DagRun.execution_date == execution_date).first()
 
-    # DAG 실행 가져오기
-    dag_runs = DagRun.find(dag_id=dag_id, execution_date=execution_date)
-    time.sleep(random.uniform(40, 90))
-    if not dag_runs:
+    if not dag_run:
         print(f"No DAG run found for DAG ID {dag_id} at {execution_date}")
+        time.sleep(70)
         return
 
-    dag_run = dag_runs[0]  # 가장 최근의 DAG Run
-
     # 해당 DAG Run에 속한 모든 Task 인스턴스 가져오기
-    task_instances = TaskInstance.find(dag_id=dag_id, execution_date=dag_run.execution_date)
-    
+    task_instances = session.query(TaskInstance).filter(TaskInstance.dag_id == dag_id, TaskInstance.execution_date == dag_run.execution_date).all()
+
     for task_instance in task_instances:
         print(f"Task {task_instance.task_id} is in state {task_instance.state}")
    
